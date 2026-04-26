cb47363d753c41bc0ab6fb0b20fce6e152e684a2
FinesaMaxhuni <finese.maxhuni@student.uni-pr.edu>
2026-04-26 10:04:35 +0200

    Shtimi i header.php

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="sq">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>NetWave - Operator Telekomunikacioni</title>
+
+<link rel="stylesheet" href="telecomoperator.css">
+</head>
+<body>
