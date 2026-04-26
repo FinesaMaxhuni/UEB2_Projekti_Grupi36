6ac4150fb4cf16704c2da84eae6bbcc730a5f191
FinesaMaxhuni <finese.maxhuni@student.uni-pr.edu>
2026-04-26 13:14:14 +0200

    Ndryshimi i linqeve duke ju pershtatur tashme struktures se re me foldera

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <title>NetWave - Operator Telekomunikacioni</title>
 
-<link rel="stylesheet" href="/assets/css/telecomoperator.css">
+<link rel="stylesheet" href="/assets/css/telecomeoperator.css">
 
 <?php
 if(isset($pageCSS)){
