335f86d33a9ef2679d3fdf723f3f42405a9dd5e7
FinesaMaxhuni <finese.maxhuni@student.uni-pr.edu>
2026-04-27 16:16:06 +0200

    Ndryshimi i dizajnit te faqes pagesa

--- pages/pagesa.php
+++ pages/pagesa.php
@@ -104,7 +104,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/UEB1_Projekti_Grupi19/includes/navbar.php'
         <div class="success-message" id="successMessage">
           <h3>Faleminderit për porosinë tuaj!</h3>
           <p>Porosia dhe pagesa janë kryer me sukses.</p>
-          <a href="telefona.php" class="back-link">← Kthehu tek produktet</a>
+          <a href="pages/telefona.php" class="back-link">← Kthehu tek produktet</a>
         </div>
 
       </div>
