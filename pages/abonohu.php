add0952b0382b6aaa49e36cb9c9a397406a32bf5
vesashala5 <vesa.shala5@student.uni-pr.edu>
2026-04-27 16:45:08 +0200

    Shtimi i validimit RegEx per mbiemrin qe te mos kete hapesira apo simbole/numra

--- pages/abonohu.php
+++ pages/abonohu.php
@@ -17,6 +17,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (!preg_match("/^[a-zA-ZëËçÇ]{2,}$/u", $emri)) {
     $gabime[] = "Emri duhet të përmbajë vetëm shkronja pa hapësira.";
   }
+  if (!preg_match("/^[a-zA-ZëËçÇ]{2,}$/u", $mbiemri)) {
+    $gabime[] = "Mbiemri duhet të përmbajë vetëm shkronja pa hapësira.";
+  }
   if (!preg_match("/^[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,}$/", $email)) {
       $gabime[] = "Email nuk është valid.";
   }
