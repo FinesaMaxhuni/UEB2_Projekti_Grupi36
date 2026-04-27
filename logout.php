8046f71930b38fbf1b1b8e338632348a25d2afcf
vesabraina <vesa.braina@student.uni-pr.edu>
2026-04-27 19:59:02 +0200

    update logout.php

--- logout.php
+++ logout.php
@@ -9,8 +9,8 @@ $_SESSION = [];
 session_unset();
 session_destroy();
 
-setcookie("netwave_user", "", time() - 3600, "/");
-setcookie("netwave_role", "", time() - 3600, "/");
+setcookie("netwave_user", "", time() - 3600, "/", "", false, true);
+setcookie("netwave_role", "", time() - 3600, "/", "", false, true);
 
 header("Location: $page");
 exit();
