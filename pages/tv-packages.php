011e42ba33d82ac0db408f0d040be0fa0cfe78ba
FinesaMaxhuni <finese.maxhuni@student.uni-pr.edu>
2026-05-11 12:43:45 +0200

    Ndryshime

--- pages/tv-packages.php
+++ pages/tv-packages.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< rela
-=======
-
->>>>>>> main
 $gabime = [];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -11,17 +7,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $telefon = $_POST["telefon"] ?? "";
 
     if (!preg_match("/^[a-zA-ZëËçÇ\s]{2,}$/u", $emri)) {
-<<<<<<< rela
-        $gabime[] = "Emri duhet të përmbajë vetëm shkronja dhe të ketë të paktën 2 karaktere.";
-    }
-
-    if (!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/", $email)) {
-        $gabime[] = "Email duhet të përmbajë @ dhe të përfundojë me domain si .com, .org, .net ose .edu.";
-    }
-
-    if (!preg_match("/^\+?[0-9\s]{9,15}$/", $telefon)) {
-        $gabime[] = "Numri i telefonit duhet të ketë 9 deri 15 shifra dhe mund të fillojë me +.";
-=======
         $gabime[] = "Emri nuk është valid.";
     }
 
@@ -31,7 +16,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (!preg_match("/^\+?[0-9\s]{9,15}$/", $telefon)) {
         $gabime[] = "Numri i telefonit nuk është valid.";
->>>>>>> main
     }
 
     if (empty($gabime)) {
@@ -39,11 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-<<<<<<< rela
-$pageCSS = "tv-packages.css?v=validation-style-3";
-=======
 $pageCSS = "tv-packages.css";
->>>>>>> main
 
 require $_SERVER['DOCUMENT_ROOT'] . '/UEB2_Projekti_Grupi36/config.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/UEB2_Projekti_Grupi36/includes/header.php';
